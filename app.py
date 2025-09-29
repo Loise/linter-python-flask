f341a002afac9983d0ddbb8636e13222b9005117
myuuu <myuuu@live.fr>
2025-09-29 17:35:30 +0200

    init flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health_check():
+    return jsonify({"status": "ok"})
+
+@app.route("/hello")
+def hello():
+    return jsonify({"message": "Hello world"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+    
